6ddc761bd994b99639deadb9bb0c3edc4b02f3f0
Ewelina Nowak <ewelinanowak@Ewelinas-MacBook-Pro.local>
2023-01-18 11:53:39 +0100

    first commit

---
+++ 12-http.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Welcome to our home page");
+  }
+
+  if (req.url === "/about") {
+    res.end("about us");
+  }
+
+  res.end(
+    `<h1>Oops!</h1>
+    <a href="/">back home</a>
+    `
+  );
+});
+
+server.listen(5000);
